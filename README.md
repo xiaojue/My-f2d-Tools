cf373a35d32fb0c4387bb9d738bb21d0ec01a681
xiaojue <designsor@gmail.com>
2012-03-30 07:53:14 +0400

    add test

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 rebuild the css file or directory.
   
 ```bash
-python cssbuild.py test.css test-min.css --build
+python cssbuild.py test/build/test.css test/build/test-min.css --build
 ``` 
 All the @import files will build in test-min.css
   
 ```bash
-python cssbuild.py test.css test-min.css --compress
+python cssbuild.py test/compress/test.css test/compress/test-min.css --compress
 ```
 It will be compress the test.css to test-min.css
 
 ```bash
-python cssbuild.py test/ test-min/ --build
-python cssbuild.py test/ test-min/ --compress
+python cssbuild.py test/build test/build-min --build
+python cssbuild.py test/compress test/compress-min --compress
 ```
 
 Supported directory
--- cssbuild.py
+++ cssbuild.py
@@ -5,7 +5,7 @@ root = os.path.abspath('.')
 src = sys.argv[1]
 target = sys.argv[2]
 options = sys.argv[3]
-yuicompressor = root+ '\\yuicompressor\\yuicompressor.jar'
+yuicompressor = root+ '/yuicompressor/yuicompressor.jar'
 
 def concatCss(modules,conf):
   conf = os.path.normpath(conf)
@@ -13,10 +13,10 @@ def concatCss(modules,conf):
     f = open(conf,'r+')
     if not os.path.isdir('temp'):
       os.mkdir('temp')
-    temp = file(root+'\\'+'temp'+'\\'+os.path.basename(conf),'w')
+    temp = file(root+'/'+'temp'+'/'+os.path.basename(conf),'w')
     allstr = ''
     for i in modules:
-      ff = open(os.path.normpath(root+'\\'+i),'r+')
+      ff = open(os.path.normpath(root+'/'+i),'r+')
       strs = ff.read()
       strs = re.sub('\@charset \"utf\-8\"\;','',strs)
       allstr += strs
@@ -29,7 +29,7 @@ def concatCss(modules,conf):
 def getcssimports(conf):
   modules = []
   conf = os.path.normpath(conf)
-  confdir = os.path.dirname(conf)+'\\'
+  confdir = os.path.dirname(conf)+'/'
   f = open(conf,'r+')
   strs = f.read()
   temp = re.findall('\@import url\(\"(.*?)\"\)',strs,re.S)
@@ -45,17 +45,17 @@ def batchCss(conf):
 def buildCss(src,target):
   if os.path.isfile(src):
     batchCss(src)
-    os.popen('java -jar '+yuicompressor+' --charset utf-8 '+root+'\\'+'temp'+'\\'+os.path.basename(src)+' -o '+root+'\\'+target)
+    os.popen('java -jar '+yuicompressor+' --charset utf-8 '+root+'/'+'temp'+'/'+os.path.basename(src)+' -o '+root+'/'+target)
     if os.path.isdir('temp'):
       shutil.rmtree('temp')
   elif os.path.isdir(src):
     if not os.path.isdir(target):
       os.mkdir(target)
     for i in os.listdir(src):
-      css = os.path.normpath(src +'\\'+i)
+      css = os.path.normpath(src +'/'+i)
       batchCss(css)
       name = os.path.basename(css)
-      os.popen('java -jar '+yuicompressor+' --charset utf-8 '+root+'\\'+'temp'+'\\'+name+' -o '+root+'\\'+target+'\\'+name)
+      os.popen('java -jar '+yuicompressor+' --charset utf-8 '+root+'/'+'temp'+'/'+name+' -o '+root+'/'+target+'/'+name)
     if os.path.isdir('temp'):
       shutil.rmtree('temp')
   else:
@@ -68,15 +68,19 @@ def compressCss(src,target):
     if not os.path.isdir(target):
       os.mkdir(target)
     for i in os.listdir(src):
-      css = os.path.normpath(src +'\\'+i)
+      css = os.path.normpath(src +'/'+i)
       name = os.path.basename(css)
-      os.popen('java -jar '+yuicompressor+' --charset utf-8 '+css+' -o '+root+'\\'+target+'\\'+name)
+      os.popen('java -jar '+yuicompressor+' --charset utf-8 '+css+' -o '+root+'/'+target+'/'+name)
   else:
     print src+' not exists'
 
 if options=='--build':
+  src = os.path.normpath(src)
+  target = os.path.normpath(target)
   buildCss(src,target)
 if options=='--compress':
+  src = os.path.normpath(src)
+  target = os.path.normpath(target)
   compressCss(src,target)
   
 
